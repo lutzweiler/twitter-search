807648b578e181ee4825dadd6942075e5fa99153
Julia Lutzweiler <uodux@student.kit.edu>
2022-06-26 20:36:41 +0200

    added weighted prioritization

--- src/main.py
+++ src/main.py
@@ -5,35 +5,38 @@ import priority
 import request
 import tweet
 
-token_file = open("bearer_token")
-bearer_token = token_file.readline().rstrip()
-
-properties = request.RequestProperties()
-properties.bearer_token = bearer_token
-properties.search_terms = ["phishing", "phishen", "gefisht"]
-properties.max_results = 50
-
-request = request.TwitterRequest(properties)
-request.execute()
-result = request.get_result()
-
-# serialize
-with open('query_result.json', 'w') as f:
-    s = jsonpickle.encode(result)
-    f.write(s)
+#token_file = open("bearer_token")
+#bearer_token = token_file.readline().rstrip()
+#
+#properties = request.RequestProperties()
+#properties.bearer_token = bearer_token
+#properties.search_terms = ["phishing", "phishen", "gefisht"]
+#properties.max_results = 500
+#
+#request = request.TwitterRequest(properties)
+#request.execute()
+#result = request.get_result()
+#
+## serialize
+#with open('../data/query_result.json', 'w') as f:
+#    s = jsonpickle.encode(result)
+#    f.write(s)
 
 #Storing the tweets in a file and then loading them again does not make much sense now,
 #but will be useful to compare different priority functions on the same data set
  
 tweets = []
 # deserialize
-with open('query_result.json', 'r') as f:
+with open('../data/query_result.json', 'r') as f:
     s = f.read()
     tweets = jsonpickle.decode(s)
 
-priority_function = priority.UniformPriorityFunction
+priority_function = priority.WeightedPriorityFunction
 priority = priority.PriorityCalculator(tweets, priority_function)
 priority.calculate_priorities()
+priority.sort_by_priority()
+tweets = priority.get_tweet_list()
 
 for tweet in tweets:
     print(tweet)
+    print("-----------")
--- src/priority.py
+++ src/priority.py
@@ -1,4 +1,6 @@
 import tweet
+import math
+import datetime
 
 class PriorityCalculator:
     def __init__(self, tweets, priority_function):
@@ -9,6 +11,44 @@ class PriorityCalculator:
         for tweet in self.tweets:
             tweet.priority = self.priority_function(tweet)
 
+    def sort_by_priority(self):
+        self.tweets = sorted(self.tweets, key = lambda tweet: tweet.priority, reverse = True)
+
+    def get_tweet_list(self):
+        return self.tweets
+
 #Example Function, gives every tweet the same priority
 def UniformPriorityFunction(tweet):
     return 1
+
+def WeightedPriorityFunction(tweet):
+    #define weights
+    w_time = -0.02
+    w_followers = 1.0
+    w_verified = 5.0
+    w_likes = 1.0
+    w_retweets = 5.0
+    w_responses = 2.0
+    w_quotes = 2.0
+
+    now = datetime.datetime.now().astimezone()
+
+    tweet_age = (now - tweet.time).total_seconds() / 3600 #in hours
+    followers = math.log(1 + tweet.user_num_followers)
+    verified = 1 if tweet.user_is_verified else 0
+    likes = math.log(1 + tweet.num_likes)
+    retweets = math.log(1+ tweet.num_retweets)
+    responses = math.log(1+ tweet.num_responses)
+    quotes = math.log(1+ tweet.num_quotes)
+
+    priority = 0
+    priority += w_time * tweet_age
+    priority += w_followers * followers
+    priority += w_verified * verified
+    priority += w_likes * likes
+    priority += w_retweets * retweets
+    priority += w_responses * responses
+    priority += w_quotes * quotes
+
+    return priority
+
